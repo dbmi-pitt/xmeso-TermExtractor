315385de3d76a29b3de09ba8598d43c2502ebf06
chb69 <chb69@pitt.edu>
2016-08-12 10:39:55 -0400

    initial checkin

---
+++ src/edu/pitt/dbmi/nmvb/nlp/TermExtractor/dbconf_example.py
@@ -0,0 +1,20 @@
+'''
+Created on Sep 14, 2015
+
+@author: chb69
+'''
+dbms = 'Oracle'
+# dbms = 'SQL Server'
+
+# Oracle Server use
+dbHost = ''
+dbPort = 1521 
+dbSid =  '' # For Oracle only
+dbUser = ''
+dbSchema = ''
+dbPassword = ''
+
+umls_api_key = ''
+umlsUsername = ''
+umlsPassword = ''
+outputDir = ''
